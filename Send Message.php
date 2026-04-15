0919059b291781103ee515c72b00dfa3b92a8966
obahiagbondaniel <obahiagbondaniel@gmail.com>
2026-04-15 13:37:52 +0100

    Add files via upload

---
+++ Send Message.php
@@ -0,0 +1,2 @@
+<!-- Now lets first create the DATABASE -- using our SQL Query we are have in file txt -->
+
--- js/script.js
+++ js/script.js
@@ -7,6 +7,21 @@
 
 
 /**
+ * PRELOAD
+ * 
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
  *light and dark mode 
  */
 
